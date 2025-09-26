668fd2fef1cc3042854cc215c943cb901d926f91
charukirti <charukirticc112002@gmail.com>
2025-09-26 23:21:44 +0530

    feat: create snippet model

---
+++ src/models/snippet.model.ts
@@ -0,0 +1,18 @@
+import mongoose, { Schema } from "mongoose";
+
+const snippetSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    code: { type: String, required: true },
+    language: { type: String, required: true },
+    tags: { type: [String], required: true },
+    userId: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+
+export const SnippetModel = mongoose.model('Snippet', snippetSchema)
