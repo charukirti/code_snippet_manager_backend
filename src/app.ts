cbe594ef91f660182b17b97d3c7d1d06942ea14e
charukirti <charukirticc112002@gmail.com>
2025-09-27 00:10:35 +0530

    feat: integrate Clerk authentication middleware
    - install @clerk/express package
    - add clerk middleware to app.ts
    - create protectedRotue helper middleware
    - configure environment variables

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,21 @@
 import express from 'express'
 import globalErrorHandler from "./middlewares/globalErrorHandler.js";
+import {clerkMiddleware} from '@clerk/express'
 
 const app = express();
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use(clerkMiddleware())
 
 app.get("/health", (req, res) => {
   res.json({ message: "Welcome to the api" });
+});
+app.get('/', (req, res) => {
+  res.json({ 
+    message: 'Code Snippets API is running!',
+    version: '1.0.0',
+  });
 });
 
 
--- src/config/config.ts
+++ src/config/config.ts
@@ -4,7 +4,9 @@ conf()
 const _config = {
     port: process.env.PORT,
     databaseUrl: process.env.MONGO_CONNECTION_STRING,
-    env: process.env.NODE_ENV
+    env: process.env.NODE_ENV,
+    clerk_publish: process.env.CLERK_PUBLISHABLE_KEY,
+    clerk_secret: process.env.CLERK_SECRET_KEY
 }
 
 export const config = Object.freeze(_config)
