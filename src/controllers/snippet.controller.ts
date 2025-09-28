06ca3128ba8fba4c38b32c85d491eb9c129c24a8
charukirti <charukirticc112002@gmail.com>
2025-09-28 14:07:01 +0530

    feat(crud): implement getSnippetById controller
    - add proper error handling

--- src/controllers/snippet.controller.ts
+++ src/controllers/snippet.controller.ts
@@ -45,7 +45,27 @@ export async function getSnippetById(
   req: Request,
   res: Response,
   next: NextFunction
-) {}
+) {
+  try {
+    const id = req.params.id;
+
+    const { userId } = getAuth(req);
+
+    const snippet = await SnippetModel.findOne({ _id: id, userId: userId });
+
+    if (!snippet) {
+      return next(new AppError("Snippet not found", 404));
+    }
+
+    console.log("Single snippet", snippet);
+
+    res.status(200).json({ success: true, data: snippet });
+  } catch (error) {
+    console.log("Unable to get snippet", error);
+
+    return next(new AppError("Unable to fetch snippet", 500));
+  }
+}
 
 // create new snippet
 
@@ -74,7 +94,7 @@ export async function createSnippet(
 
     await SnippetModel.create(newSnippet);
 
-    return res.status(201).json({success: true, title: newSnippet.title });
+    return res.status(201).json({ success: true, title: newSnippet.title });
   } catch (error) {
     console.error("Create snippet error:", error);
     return next(new AppError("Error while adding new snippet", 500));
