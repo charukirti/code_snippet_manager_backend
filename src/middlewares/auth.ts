48c322c0b5e5897506ac1e7eaa4035eb36148fd7
charukirti <charukirticc112002@gmail.com>
2025-09-30 09:34:50 +0530

    feat(middleware): create auth middleware to reduce code duplication - Create checkAuth middleware to extract and validate userId - Extend Express REQUEST type to include userId properly

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,3 +1,17 @@
-import { requireAuth } from "@clerk/express";
+import { getAuth, requireAuth } from "@clerk/express";
+import { AppError } from "../types/index.js";
+import type { Request, Response, NextFunction } from "express";
 
 export const protectRoute = requireAuth();
+
+export const checkAuth = (req: Request, res: Response, next: NextFunction) => {
+  const { userId } = getAuth(req);
+
+  if (!userId) {
+    throw new AppError("Not authenticated", 401);
+  }
+
+  req.userId = userId;
+
+  next();
+};
