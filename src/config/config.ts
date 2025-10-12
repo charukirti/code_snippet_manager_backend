6af246b9f3429a1a6ff360da3e2c1bc059f9283b
charukirti <charukirticc112002@gmail.com>
2025-10-12 10:43:29 +0530

    fix: cors issue

--- src/config/config.ts
+++ src/config/config.ts
@@ -35,7 +35,7 @@ const _config = {
   clerk_publish: env.CLERK_PUBLISHABLE_KEY,
   clerk_secret: env.CLERK_SECRET_KEY,
   client_url:
-    env.NODE_ENV === "development" ? "http://localhost:3000" : env.CLIENT_URL,
+    env.NODE_ENV === "development" ? "https://code-snippet-manager-eight.vercel.app" : "http://localhost:3000",
 };
 
 export const config = Object.freeze(_config);
